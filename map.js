577ebe899d68dbf3a2385646fa5b5e461093b378
Yana Yakubchik <Yana Yakubchik>
2016-06-21 18:30:28 -0400

    got a map and location to show up

--- map.js
+++ map.js
@@ -1,15 +1,31 @@
- $(document).ready(function() { 
-var queryURL = "http://api.trulia.com/webservices.php?library=LocationInfo&function=getCitiesInState&state=CA&apikey=c72wfgjsbeng8ya6mfttdz8b";
+$(document).ready(function() {
 
- $.ajax({url: queryURL, method: 'GET'})
-           //call back function
-           .done(function(response) {
 
-           console.log(queryURL);
-           console.log(response); 
 
-           $('#here').html(JSON.stringify(response));
+  //trulia api 
+  var apiKey = "c72wfgjsbeng8ya6mfttdz8b";
+  var queryURL = "https://crossorigin.me/http://api.trulia.com/webservices.php?library=TruliaStats&function=getCityStats&city=New York&state=NY&startDate=2009-02-06&endDate=2009-02-07&apikey=" + apiKey;
+   console.log(queryURL);
 
-           }); 
+  
+  $.ajax({
+      url: queryURL,
+      dataType: 'xml',
+      method:'GET'
+  })
 
-})
+  .done(function(response){
+    console.log('response', response);
+    var json = $.xml2json(response);
+    //
+
+  
+     console.log(json);
+     $('#here').append(JSON.stringify(json.response.TruliaStats.location.state));
+
+      $('#there').append(JSON.stringify(json.response.TruliaStats.location.city));
+  })
+
+
+
+});
